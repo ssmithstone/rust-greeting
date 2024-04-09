708127bcfe138c84db6274ec44402d2bd0c1ada7
stephen smithstone <ssmithstone@me.com>
2024-04-10 05:58:56 +1000

    Add chrono dependency and current time functionality
    A new dependency, chrono version 0.4.37, has been added to the Cargo.toml file to handle dates and times. In the main.rs file, the chrono library is used to create a new function called print_current_time, which prints formatted time and date information.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,26 @@
 use std::env;
+use chrono::{Datelike, Local, Timelike};
 
 fn main() {
     print_greeting();
 
+    print_current_time();
+}
+
+fn print_current_time() {
+    let now = Local::now();
+    println!("Now = {}", now.timestamp());
+    println!("Now Formatted = {}", now.to_rfc2822());
+    let date = now.date_naive();
+    let month = date.month();
+    let year = date.year();
+    let day = date.day();
+    println!("Year: {}\nMonth: {}\nDay: {}", year, month, day);
+    let time = now.time();
+    let hour = time.hour();
+    let minutes = time.minute();
+    let seconds = time.second();
+    println!("Hour: {}\nMinute: {}\nSeconds: {}", hour, minutes, seconds);
 }
 
 fn print_greeting() {
