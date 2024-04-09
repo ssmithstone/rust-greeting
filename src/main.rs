273b78388d2df86c8815fe1286fe7c39c898ead6
stephen smithstone <ssmithstone@me.com>
2024-04-10 05:51:55 +1000

    Add argument handling to main function
    The code now accepts command line arguments. If at least one argument is provided, it will be taken as a 'name' and printed with a greeting. If no arguments are provided, the program defaults to greeting a hardcoded name, "Luke Skywalker".

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    print_greeting();
+
+}
+
+fn print_greeting() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        let name = "Luke Skywalker".to_string();
+        println!("Hello, {}!", name);
+    } else {
+        let name = args.get(1);
+        match name {
+            Some(n) => println!("Hello, {}!", n),
+            None => panic!(),
+        }
+    }
 }
